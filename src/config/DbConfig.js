bf72036d26215088135f73c5bff85a6158c5818e
suyashLade <suyashlade@gmail.com>
2023-07-28 12:20:43 +0530

    username check request

--- src/config/DbConfig.js
+++ src/config/DbConfig.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 export async function DbConnect(url) {
   try {
-    await mongoose.connect(url).then(console.log("server listening at 4000"));
+    await mongoose.connect(url).then(console.log("server listening at 3001"));
   } catch (err) {
     console.error(err);
     throw err;
--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -1,11 +1,14 @@
 import { Router } from "express";
-import {  Login, Logout, Register } from "../controllers/auth.controllers.js";
-import { ValidateLoginSchema, ValidateUserSchema } from "../model/User.model.js";
+import { Login, Logout, Register } from "../controllers/auth.controllers.js";
+import {
+  ValidateLoginSchema,
+  ValidateUserSchema,
+} from "../model/User.model.js";
 
 const router = Router();
 
-router.post("/register",ValidateUserSchema, Register);
-router.patch('/updateuser',)
-router.post("/login",ValidateLoginSchema, Login);
-router.get('/logout',Logout)
+router.post("/register", ValidateUserSchema, Register);
+router.patch("/updateuser");
+router.post("/login", ValidateLoginSchema, Login);
+router.get("/logout", Logout);
 export default router;
