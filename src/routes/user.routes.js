d030882497b2442d98f6d5f840da2ea9e0ba739c
suyashLade <suyashlade@gmail.com>
2023-07-28 19:41:33 +0530

    added middleware

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,8 +1,9 @@
 import { Router } from "express";
 import { CheckUserName } from "../controllers/user.controllers.js";
+import PermissionCheck from "../middleware/permissionCheck.js";
 
 const router = Router();
 
-router.post("/searchUsername", CheckUserName);
+router.post("/searchUsername", PermissionCheck("user"), CheckUserName);
 
 export default router;
